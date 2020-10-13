bcbc3fa939d0bbff0899c9d282aed96629561d4b
Jake Ascher <ascherj@gmail.com>
2020-10-13 15:45:21 -0400

    Modify custom hook to send order data

--- starter-files/gatsby/src/utils/usePizza.js
+++ starter-files/gatsby/src/utils/usePizza.js
@@ -1,10 +1,16 @@
 import { useContext, useState } from 'react';
 import OrderContext from '../components/OrderContext';
+import attachNamesAndPrices from './attachNamesAndPrices';
+import calculateOrderTotal from './calculateOrderTotal';
+import formatMoney from './formatMoney';
 
-export default function usePizza({ pizzas, inputs }) {
+export default function usePizza({ pizzas, values }) {
   // below line not necessary due to moving state up to provider
   // const [order, setOrder] = useState([]);
   const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   function addToOrder(orderedPizza) {
     setOrder([...order, orderedPizza]);
@@ -14,11 +20,49 @@ export default function usePizza({ pizzas, inputs }) {
     setOrder([...order.slice(0, index), ...order.slice(index + 1)]);
   }
 
-  // TODO: Send data to a serverless function upon checkout
+  async function submitOrder(e) {
+    e.preventDefault();
+    console.log(e);
+    setLoading(true);
+    setError(null);
+    setMessage(null);
+    // gather data
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+    };
+
+    // send data to a serverless function upon checkout
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const text = JSON.parse(await res.text());
+
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false);
+      setError(text.message);
+    } else {
+      setLoading(false);
+      setMessage('Success! Come on down for your pizza!');
+    }
+  }
 
   return {
     order,
     addToOrder,
     removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
   };
 }
